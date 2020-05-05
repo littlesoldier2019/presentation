89a09421b5fc5a5d454df770f64b804e741cd96b
littlesoldier2019 <hongngoc.ftu2011@gmail.com>
2020-05-06 00:33:51 +0300

    add jira image

--- src/components/Designs.js
+++ src/components/Designs.js
@@ -3,13 +3,15 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import Slide1 from '../image/h1.jpg';
 import Slide2 from '../image/h2.jpg';
-import Slide3 from '../image/h3.jpg';
+import Slide3 from '../image/h3.png';
 import Slide4 from '../image/h4.jpg';
+import Slide5 from '../image/h5.png';
+import Slide6 from '../image/h6.png';
 
 
 
 function Designs () {
-    const [slides, setSlide] = useState([Slide1, Slide2, Slide3, Slide4])
+    const [slides, setSlide] = useState([Slide1, Slide2, Slide3, Slide4, Slide5, Slide6])
     const content = slides.map(item => {
         return (
             <div style={{ backgroundImage: `url(${item})`,  backgroundPosition:'center', backgroundRepeat:'no-repeat', height:'95vh'}}>
@@ -20,7 +22,7 @@ function Designs () {
     return (
         <div style={{ marginLeft: '250px', height:'100vh'}}>
             <Carousel
-                // autoPlay={true}
+                autoPlay={true}
                 interval={8000}
                 stopOnHover={true}
                 infiniteLoop={true}
