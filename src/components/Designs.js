fcfbe9c57c4adfd54517cde88530cd37feb5c33e
littlesoldier2019 <hongngoc.ftu2011@gmail.com>
2020-05-06 11:21:35 +0300

    change git image

--- src/components/Designs.js
+++ src/components/Designs.js
@@ -5,13 +5,12 @@ import Slide1 from '../image/h1.jpg';
 import Slide2 from '../image/h2.jpg';
 import Slide3 from '../image/h3.png';
 import Slide5 from '../image/h5.png';
-import Slide6 from '../image/h6.png';
 import Slide7 from '../image/h7.png';
 
 
 
 function Designs () {
-    const [slides, setSlide] = useState([Slide1, Slide2, Slide3, Slide5, Slide6, Slide7])
+    const [slides, setSlide] = useState([Slide1, Slide2, Slide3, Slide5, Slide7])
     const content = slides.map(item => {
         return (
             <div style={{ backgroundImage: `url(${item})`,  backgroundPosition:'center', backgroundRepeat:'no-repeat', height:'95vh'}}>
